bdda23e55adc8f74ee96cd9f6284f1215bc3ac7a
Aaron Emery <aaron.emery@sanity.io>
2025-01-29 09:38:32 -0800

    feat: initial schemas

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,9 @@
-export const schemaTypes = []
+import {terminal} from './Terminal'
+import {route} from './Route'
+import {turnGroup} from './TurnGroup'
+import {turn} from './Turn'
+import {pullIn} from './PullIn'
+import {pullOut} from './PullOut'
+import {restroom} from './Restroom'
+
+export const schemaTypes = [terminal, route, turnGroup, turn, pullIn, pullOut, restroom]
